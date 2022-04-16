8c1e691f3ef93a647b616918623027b0c6e1329f
Hélio Carvalho <heliocarvallho@gmail.com>
2022-04-16 23:54:13 +0000

    Atualização

---
+++ src/main/java/com/bank/services/LancamentoService.java
@@ -0,0 +1,22 @@
+package com.bank.services;
+
+import java.util.List;
+
+import com.bank.model.entities.Lancamento;
+import com.bank.model.enums.StatusLancamento;
+
+
+public interface LancamentoService {
+
+	Lancamento salvar(Lancamento lancamento);
+	
+	Lancamento atualizar(Lancamento lancamento);
+	
+	void deletar(Lancamento lancamento);
+	
+	List<Lancamento> buscar( Lancamento lancamentoFiltro );
+	
+	void atualizarStatus( Lancamento lancamento, StatusLancamento status );
+	
+	void validar(Lancamento lancamento);
+}
