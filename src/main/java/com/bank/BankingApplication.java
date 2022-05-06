f6c734935389541100a718973071add0896b58d9
Hélio Carvalho <63817214+heliooak@users.noreply.github.com>
2022-05-06 20:13:51 -0300

    implementand axios

--- src/main/java/com/bank/BankingApplication.java
+++ src/main/java/com/bank/BankingApplication.java
@@ -2,10 +2,19 @@ package com.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BankingApplication {
-
+@EnableWebMvc
+public class BankingApplication implements WebMvcConfigurer {
+	
+	@Override
+	public void addCorsMappings( CorsRegistry registry ) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BankingApplication.class, args);
 	}
